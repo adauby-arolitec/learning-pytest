97cc623f74d2db216164ceea568cc5d3740ecc2d
Mason Assi <adaubyassi@arolitec.com>
2021-07-22 10:08:22 +0000

    Update basic calculation

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from maths.basic_calcuations import BasicCalculation
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_restful import reqparse
+
+from maths.basic_calcuations import BasicCalculation
+
 
 app = Flask(__name__)
 api = Api(app)
 
+parser = reqparse.RequestParser()
+parser.add_argument('first_num',type=int, default = 0)
+parser.add_argument('second_num', type=int, default = 0)
+
 class CalculationController(Resource):
 
-    def get(self, first_num:int=0, second_num:int=0):
+    def get(self):
+        args = parser.parse_args()
+        first_num = args['first_num']
+        second_num = args['second_num']
         calculation = BasicCalculation(first_num, second_num)
-        return {"add_rslt": calculation.addition(),
+        
+        return {"title":f"Basic calculations with {first_num} and {second_num}",
+        "add_rslt": calculation.addition(),
         "subs_rslt": calculation.substract(),
         "multi_rslt": calculation.multiply(),
         "div_rslt": calculation.divide()} 
