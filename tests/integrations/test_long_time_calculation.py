0053527fa2cdcd706c20832c5cda7c257a34a4b7
Mason Assi <adaubyassi@arolitec.com>
2021-07-29 13:49:38 +0000

    pytest-cov integration

--- tests/integrations/test_long_time_calculation.py
+++ tests/integrations/test_long_time_calculation.py
@@ -3,32 +3,28 @@ import pytest
 
 from maths.long_time_calculation import LongTimeOperation
 
-class TestLongTimeOperation():
+#class TestLongTimeOperation():
   
-    @pytest.mark.integration_test
-    def test_of_positive_numbers(self):
-        longCalculation = LongTimeOperation
-        assert longCalculation.calculate(1,1,1) == 2
+@pytest.mark.integration_test
+def test_of_positive_numbers():
+    assert LongTimeOperation.calculate(1,1,1) == 2
         
-    @pytest.mark.integration_test    
-    def test_of_zero_numbers(self):
-        longCalculation = LongTimeOperation
-        assert longCalculation.calculate(0,0,0) == 0
+@pytest.mark.integration_test    
+def test_of_zero_numbers():
+    assert LongTimeOperation.calculate(0,0,0) == 0
         
-    @pytest.mark.integration_test    
-    def test_of_positive_and_negative_numbers(self):
-        longCalculation = LongTimeOperation
-        assert longCalculation.calculate(2,-4,-2) == 10
+@pytest.mark.integration_test    
+def test_of_positive_and_negative_numbers():
+    assert LongTimeOperation.calculate(2,-4,-2) == 10
         
-    @pytest.mark.integration_test    
-    def test_of_negative_numbers(self):
-        longCalculation = LongTimeOperation
-        assert longCalculation.calculate(-2,-4,-2) == 6
+@pytest.mark.integration_test    
+def test_of_negative_numbers():
+    assert LongTimeOperation.calculate(-2,-4,-2) == 6
+
         
-    @pytest.mark.integration_test    
-    def test_of_mixed_numbers(self):
-        longCalculation = LongTimeOperation
-        assert longCalculation.calculate(0,3,-2) == -6
+@pytest.mark.integration_test    
+def test_of_mixed_numbers():
+    assert LongTimeOperation.calculate(0,3,-2) == -6
 
       
     
